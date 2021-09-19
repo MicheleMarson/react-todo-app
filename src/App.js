92737da02f611b489ade37d58bbeab9ac8610507
Mihaela Marson <mitchmarson666@gmail.com>
2021-09-19 19:07:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
 
 function App() {
+  const [inputDate, setInputDate] = useState("")
+  const [newTask, setNewTask] = useState("") // set new task
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="app__title">Todo App</h1>
+      <form onSubmit={handleSubmit.bind(this)} className="app__inputBox">
+        <div className="app__input">
+          <div id="input-wrap">
+            <input autoComplete="off" value={newTask} type="text" id="input-task" 
+            placeholder="Add new task"  onChange={(e) => setNewTask(e.target.value)}/>
+            <input onChange={(e) => setInputDate(e.target.value)} value={inputDate} type="date" id="input-date" />
+          </div>
+          <div id="input-border"></div>
+        </div>
+        <button type="submit" className="app__add">
+          <div id="plus-h"></div>
+          <div id="plus-w"></div>
+        </button>
+      </form>
     </div>
   );
 }
