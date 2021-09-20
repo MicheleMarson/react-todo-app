e325670e6cbd0b4f22136c60a19f4c906c722b4c
Mihaela Marson <mitchmarson666@gmail.com>
2021-09-20 02:09:49 +0200

    new commit

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useState } from 'react'
+import "../style/box.css"
+import Todo from './Todo'
 
 const Box = ({todo, setTodo}) => {
   const [completed, setCompleted] = useState(false)
   const [APIDate, setAPIDate] = useState([])
+
   const handleCompleted = () => {
     setCompleted(!completed)
     setTodo({...todo, completed:completed})
@@ -12,8 +15,27 @@ const Box = ({todo, setTodo}) => {
 
   return (
     <div className="box">
-      <div>todo</div>
-      <button onClick={handleCompleted}>set completed</button>
+      <form className="box__search">
+        <input placeholder="search..." />
+        <div className="box__select">
+          <div className="box__select-date">
+            <select name="date" className="date">
+              <option value="newest">newest</option>
+              <option value="oldest">oldest</option>
+            </select>
+          </div>
+          <div className="box__select-completed">
+            <select>
+              <option value="">completed</option>
+              <option value="">not completed</option>
+            </select>
+          </div>
+        </div>
+        <div className="box__bottom"></div>
+      </form>
+      <section className="box__todos">
+        <Todo />
+      </section>
     </div>
   )
 }
